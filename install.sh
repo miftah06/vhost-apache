9be7e9c9ca57750a7c4f524d78107022acc6495b
miftah06 <izharuddinmiftah@gmail.com>
2024-03-03 23:14:45 +0800

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Install Apache2
+sudo apt install curl -y && apt install wget -y
 sudo apt-get update
 sudo apt-get install apache2 -y
+curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
+sudo apt install php8.2 php8.2-cli php8.2-{bz2,curl,mbstring,intl,zip,xml,cgi,gd,mysql}
+sudo apt install php7.4 libapache2-mod-php7.4 php7.4-cli
+sudo apt install php7.4 php7.4-cli php7.4-{bz2,curl,mbstring,intl,zip,xml,cgi,gd,mysql}
+apt install php-redis-all-dev
+apt search php | grep 8.2
 #!/bin/bash
 
 # Install Webmin
